7af13f053ce66482aec4a308f369c143728c64d5
crion99 <3527628634@qq.com>
2026-03-17 16:31:13 +0800

    3.17

--- exercises/02_no_std_dev/05_fd_table/src/lib.rs
+++ exercises/02_no_std_dev/05_fd_table/src/lib.rs
@@ -49,6 +49,7 @@ pub trait File: Send + Sync {
 /// File descriptor table
 pub struct FdTable {
     // TODO: Design the internal structure
+    structure: Vec<Option<Arc<dyn File>>>,
     // Hint: use Vec<Option<Arc<dyn File>>>
     //       the index is the fd number, None means the fd is closed or unallocated
 }
@@ -57,33 +58,48 @@ impl FdTable {
     /// Create an empty fd table
     pub fn new() -> Self {
         // TODO
-        todo!()
+        Self {
+            structure: Vec::new(),
+        }
     }
 
     /// Allocate a new fd, return the fd number.
     ///
     /// Prefers reusing the smallest closed fd number; if no free slot, appends to the end.
     pub fn alloc(&mut self, file: Arc<dyn File>) -> usize {
-        // TODO
-        todo!()
+        for (i, slot) in self.structure.iter_mut().enumerate() {
+        if slot.is_none() {
+            *slot = Some(file.clone());
+            return i;
+        }
+    }
+    self.structure.push(Some(file));
+    self.structure.len() - 1
+
     }
 
     /// Get the file object for an fd. Returns None if the fd doesn't exist or is closed.
     pub fn get(&self, fd: usize) -> Option<Arc<dyn File>> {
-        // TODO
-        todo!()
+        if fd < self.structure.len() {
+            self.structure[fd].clone()
+        } else {
+            None
+        }
     }
 
     /// Close an fd. Returns true on success, false if the fd doesn't exist or is already closed.
     pub fn close(&mut self, fd: usize) -> bool {
-        // TODO
-        todo!()
+        if fd < self.structure.len() && self.structure[fd].is_some() {
+            self.structure[fd] = None;
+            true
+        } else {
+            false
+        }
     }
 
     /// Return the number of currently allocated fds (excluding closed ones)
     pub fn count(&self) -> usize {
-        // TODO
-        todo!()
+        self.structure.iter().filter(|slot| slot.is_some()).count() 
     }
 }
 
--- exercises/03_os_concurrency/02_atomic_ordering/src/lib.rs
+++ exercises/03_os_concurrency/02_atomic_ordering/src/lib.rs
@@ -39,8 +39,9 @@ impl FlagChannel {
     /// - What Ordering should be used for writing ready? (ensuring data writes are visible to consumer)
     pub fn produce(&self, value: u32) {
         // TODO: Store data (choose appropriate Ordering)
+        self.data.store(value,Ordering::Relaxed);
         // TODO: Set ready = true (choose appropriate Ordering so data writes complete before this)
-        todo!()
+        self.ready.store(true, Ordering::Release);
     }
 
     /// Consumer: spin-wait for ready flag, then read data.
@@ -50,8 +51,11 @@ impl FlagChannel {
     /// - What Ordering should be used for reading data?
     pub fn consume(&self) -> u32 {
         // TODO: Spin-wait for ready to become true (choose appropriate Ordering)
+        while !self.ready.load(Ordering::Acquire) {
+            std::hint::spin_loop();
+        }
         // TODO: Read data (choose appropriate Ordering)
-        todo!()
+        self.data.load(Ordering::Acquire)
     }
 
     /// Reset channel state
@@ -82,14 +86,29 @@ impl OnceCell {
     /// Hint: use `compare_exchange` to ensure only one thread succeeds.
     pub fn init(&self, val: u32) -> bool {
         // TODO: Use compare_exchange to ensure initialization only once
+        if self.initialized
+            .compare_exchange(false, true, Ordering::SeqCst, Ordering::SeqCst)
+            .is_ok()
+            {
+                 self.value.store(val, Ordering::SeqCst);
+                 true
+            }
+            else {
+                false
+            }
         // Store value on success
-        todo!()
+       
+           
     }
 
     /// Get value. Returns Some if initialized, otherwise None.
     pub fn get(&self) -> Option<u32> {
         // TODO: Check initialized flag, then read value
-        todo!()
+        if self.initialized.load(Ordering::SeqCst) {
+            Some(self.value.load(Ordering::SeqCst))
+        } else {
+            None
+        }
     }
 }
 
